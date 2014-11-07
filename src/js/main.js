09f13f8afe463c65ef4d423ce6213e12963ae896
antoinegrelard <antoine.grelard@gmail.com>
2014-11-07 01:02:26 +0100

    game almost done without OOP Javascript and needs a bug fix for the delay

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,8 @@ $(document).ready(function() {
 	"use strict";
 
 	var list = $(".cards_list");
-
-	function createCards(quantity) {
-		for(var i = 0 ; i < quantity; i++) {
-			list.append($("<li class='cards_list-item'></li>"));
-		}
-	};
+	var visibleCards = [];
+	var nbClicks = 0;
 
 	var dir = "src/img/cards";
 	var fileextension=".png";
@@ -20,12 +16,67 @@ $(document).ready(function() {
 			//Lsit all png file names in the page
 			$(data).find("a:contains(" + fileextension + ")").each(function () {
 				cardsArray.push(this.title);
+				cardsArray.push(this.title);
 			});
 		}
 	});
 
-	createCards((cardsArray.length)*2);
+	function createCards(quantity) {
+		for(var i = 0 ; i < quantity; i++) {
+			list.append($("<li class='cards_list-item'></li>"));
+		}
+	}
 
-	console.log(_.shuffle(cardsArray));
+	createCards(_.size(cardsArray));
+
+	var shuffledArray = _.shuffle(cardsArray);
+
+	var counter = 0;
+	$(".cards_list-item").each(function() {
+		$(this).attr('data-image','src/img/cards/' + shuffledArray[counter] );
+		counter++;
+	});
+
+	function matchCards(first, second) {
+		var firstImage = first.data('image');
+		var secondImage = second.data('image');
+
+		if(firstImage == secondImage) {
+			_.delay(function() {
+				visibleCards= [];
+				first.attr('style', 'background: rgba(0,0,0,.2');
+				second.attr('style', 'background: rgba(0,0,0,.2');
+				first.removeClass("visible").addClass('inactive');
+				second.removeClass("visible").addClass('inactive');
+				first.unbind('click');
+				second.unbind('click');
+			}, 500);
+		} else {
+			_.delay(function() {
+				visibleCards= [];
+				first.removeClass("visible");
+				first.removeAttr('style');
+				second.removeClass("visible");
+				second.removeAttr('style');
+			}, 500);
+		}
+	}
+
+	$(".cards_list-item").on("click", function() {
+		nbClicks++;
+		$(".click-number").text(nbClicks);
+		if($(this).hasClass("visible")) {
+			$(this).removeClass("visible");
+			$(this).removeAttr('style');
+			visibleCards = [];
+		} else {
+			$(this).addClass("visible");
+			visibleCards.push($(this));
+			$(this).attr('style', 'background: url( ' + $(this).data('image') + ' )');
+		}
+		if(_.size(visibleCards) == 2) {
+			matchCards(visibleCards[0],visibleCards[1]);
+		}
+	});
 
 });
